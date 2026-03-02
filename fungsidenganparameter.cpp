e17940004b63c252f2bbe5c4afb478ceb92489fd
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-03-02 22:07:46 +0700

    commit8

---
+++ fungsidenganparameter.cpp
@@ -0,0 +1,13 @@
+// library
+#include <iostream>
+using namespace std;
+// deklarasi variabel global
+int panjang, lebar;
+// implementasi fungsi dan prosedur
+void input()
+{
+    cout << "Masukan Panajang ; ";
+    cin >> panjang;
+    cout << "Masukan Lebar : ";
+    cin >> lebar;
+}
